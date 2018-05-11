47ac3b796d80a862d53f410f82805ad518a1e124
Niskarsh Kumar <niskarshk@gmail.com>
2018-05-11 17:06:50 +0530

    Interaction within pages

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 const hbs = require("hbs");
-
+// Handlebars viewing engine
 app.set('view engine', 'hbs');
 
-
+// route set
 app.get('/', function (req, res) {
   res.render('home.hbs');
 });
@@ -17,3 +17,4 @@ app.get("/signup",(req,res)=>{
     res.render("signup.hbs");
 });
 app.listen(4000);
+// Listening
