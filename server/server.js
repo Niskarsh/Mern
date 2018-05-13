0def9319f8c0ea499fd3e3ad9105782b63b4c430
Niskarsh Kumar <niskarshk@gmail.com>
2018-05-13 19:27:42 +0530

    post req for signin and up set

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 var {mongoose} = require("./mongoose");
 var {todos} = require("./models/todos");
 var {users} = require("./models/users");
-
+const hbs = require("hbs");
 var express = require("express");
 var app = express();
+
+// Handlebars viewing engine
+app.set('view engine', 'hbs');
 var bodyparser = require("body-parser");
 app.use(bodyparser.json());
+// POST requests
+app.post("/signin",(req,res)=>{
+    
+    users.find({"username" : req.body.username}).then((user)=>{
+        console.log(user[0].username);
+        if(user[0].password===req.body.password){
+            return res.render("user_page.hbs",{
+                name : user[0].username});
+        }
+
+
+    },(e)=>{
+        res.send(res.render(e));
+
+    });
+});
+
 
 app.post("/signup",(req,res)=>{
     var user = new users({
@@ -13,12 +33,24 @@ app.post("/signup",(req,res)=>{
         password: req.body.password
     });
     user.save().then((doc)=>{
-        res.send(doc);
+        res.render("signin.hbs");
     },(e)=>{
         res.send(e);
     });
+    
+});
+// GET Requests
+app.get('/', function (req, res) {
+    res.render('home.hbs');
+  });
+  
+app.get("/signin",(req,res)=>{
+      res.render("signin.hbs");
 });
 
+app.get("/signup",(req,res)=>{
+    res.render("signup.hbs");
+});
 
 app.listen(4001,()=>{
     console.log("Started listening");
